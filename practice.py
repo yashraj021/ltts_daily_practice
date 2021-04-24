15a2be3c5e462c166e2f10b1ad5dd62aa8f98ee9
Yash <yash.raj021@gmail.com>
2021-04-24 21:11:42 +0530

    day 2 python practice

---
+++ practice.py
@@ -0,0 +1,32 @@
+#Data Types
+# int
+# float 
+
+print(3+2)
+print(type(5))
+
+#MAth Functions
+
+print(round(3.1))
+print(abs(-30))
+
+#Binary
+print(bin(5))
+print(int(bin(5),2))
+
+#variables
+user_id = 190
+
+print(user_id)
+
+a, b, c = 1, 2, 3 
+
+print(a, b, c)
+
+#strings
+
+print(type("HI"))
+
+print('hi' + 'brother')
+str_var = 'yash' + ' ' + ' Yash' 
+print(str_var)
